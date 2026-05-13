7c1db68db82955318fb08fa0ef24afb6e6c90fdc
NOGUESSMETHOD <noguessmethod@gmail.com>
2026-05-13 19:12:48 -0400

    Delete noguessmethod_stripe_session_and_logo_fixed directory

--- noguessmethod_stripe_session_and_logo_fixed/src/lib/supabase.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-export const supabase = createClient(
-  'https://lbchkywhvpcutwjksdno.supabase.co',
-  'sb_publishable_4uFOBOikUbOvvOuknj7qsQ_D71Xr3Ct'
-)
